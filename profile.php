f82fab4cd4026224a8730d2bd3f4ef12f2a4e0cd
ihongx <30934502+ihongx@users.noreply.github.com>
2019-03-08 23:02:32 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = '7UrEo7mB6P+Z9dB7S8yNJGOXh6siNQixgdgALVb888pVt2FjYL6p3ElUA4M4sDceWuTTCovcNLcyiN1jLt5MRm/q7d7k2jpMOn18yrzUx5GAPsrRu86DSasMYG+xKTzl8wti3EOzb+2+jscFoWtYiQdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'U926c52ab6fb6ba95cacff48d28b6d8da';
+$userId = 'U9376570e7129e8665eb35787fa03d0f8';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
